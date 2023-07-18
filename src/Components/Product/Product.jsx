8f1c08ef496a666ccf22f930414ba8618e0a03ee
webdev-alexey <alexey.anasov@yandex.by>
2023-07-18 11:22:30 +0300

    добавлены карточки новинки mainpage

---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import { API_URL } from '../../const.js';
+import { ReactComponent as Like } from '../../assets/heart.svg';
+import s from './Product.module.scss';
+import { ColorList } from '../ColorList/ColorList.jsx';
+
+export const Product = ({ id, pic, title, price, colors }) => (
+    <article className={s.product}>
+        <NavLink className={s.link} to={`product/${id}`}>
+            <img className={s.image} src={`${API_URL}${pic}`} />
+            <h3 className={s.title}>{title}</h3>
+        </NavLink>
+
+        <div className={s.row}>
+            <p className={s.price}>руб {price}</p>
+
+            <button className={s.favorite}>
+                <Like />
+            </button>
+        </div>
+        <ColorList colors={colors} />
+    </article>
+)
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import navigationReducer from './features/navigationSlice.js';
 import colorReducer from './features/colorSlice.js';
+import goodsReducer from './features/goodsSlice.js';
 
 export const rootReducer = combineReducers({
     navigation: navigationReducer,
     color: colorReducer,
+    goods: goodsReducer,
 });
